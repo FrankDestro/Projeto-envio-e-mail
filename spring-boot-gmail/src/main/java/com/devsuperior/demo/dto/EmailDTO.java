03497d8708f4fac4dee7782f8aa56f1eb7149225
FRANKLYN DESTRO DAMACENO <franklyn.damaceno@grb.local>
2024-01-10 14:55:38 -0300

    Atualizado função para envio de e-mail em massa

--- spring-boot-gmail/src/main/java/com/devsuperior/demo/dto/EmailDTO.java
+++ spring-boot-gmail/src/main/java/com/devsuperior/demo/dto/EmailDTO.java
@@ -1,40 +1,34 @@
 package com.devsuperior.demo.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
 
 public class EmailDTO {
 
-    @NotBlank
-    @Email
-    private String to;
-    
+	@NotEmpty
+	private List<String> to;
     @NotBlank
     private String subject;
-    
     @NotBlank
     private String body;
 
-	public String getTo() {
+	public List<String> getTo() {
 		return to;
 	}
-
-	public void setTo(String to) {
+	public void setTo(List<String> to) {
 		this.to = to;
 	}
-
 	public String getSubject() {
 		return subject;
 	}
-
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-
 	public String getBody() {
 		return body;
 	}
-
 	public void setBody(String body) {
 		this.body = body;
 	}
--- spring-boot-gmail/src/main/java/com/devsuperior/demo/services/EmailService.java
+++ spring-boot-gmail/src/main/java/com/devsuperior/demo/services/EmailService.java
@@ -10,26 +10,32 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.demo.dto.EmailDTO;
 import com.devsuperior.demo.services.exceptions.EmailException;
 
+import java.util.List;
+
 @Service
 public class EmailService {
-	
+
 	@Value("${spring.mail.username}")
 	private String emailFrom;
-	
-    @Autowired
-    private JavaMailSender emailSender;
 
-    public void sendEmail(EmailDTO obj) {
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(emailFrom);
-            message.setTo(obj.getTo());
-            message.setSubject(obj.getSubject());
-            message.setText(obj.getBody());
-            emailSender.send(message);
-        } 
-        catch (MailException e){
-        	throw new EmailException("Failed to send email");
-        } 
-    }
+	@Autowired
+	private JavaMailSender emailSender;
+
+	public void sendEmail(EmailDTO obj) {
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(emailFrom);
+			List<String> toList = obj.getTo();
+			String[] toArr = new String[toList.size()];
+			toArr = toList.toArray(toArr);
+			message.setTo(toArr);
+			message.setSubject(obj.getSubject());
+			message.setText(obj.getBody());
+			emailSender.send(message);
+		} catch (MailException e) {
+			e.printStackTrace();
+			throw new EmailException("Failed to send email");
+		}
+	}
+
 }
